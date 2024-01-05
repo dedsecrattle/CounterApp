e31015081fd5417f1d59edf82c05845c72b5ed3b
dedsecrattle <itsprabxxx@gmail.com>
2024-01-05 21:40:49 +0530

    Added a Comment functionality for Saving

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let count = 0
 let countEl = document.getElementById("count-el")
 let tableEl = document.getElementById("hist-el")
 let index = 1
+let commentEl = document.getElementById("save-cmnt")
 
 function increment() {
     count += 1
@@ -15,14 +16,22 @@ function reset() {
 
 
 function save(){
-    if(count > 0){
-        var row = tableEl.insertRow(index);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        cell1.textContent = index
-        cell2.textContent = count
-        reset()
-        index += 1
+    if(commentEl.value == ""){
+        alert("Comment can't be Empty !")
+    }
+    else{
+        if(count > 0){
+            var row = tableEl.insertRow(index);
+            var cell1 = row.insertCell(0)
+            var cell2 = row.insertCell(1)
+            var cell3 = row.insertCell(2)
+            cell1.textContent = index
+            cell2.textContent = count
+            cell3.textContent = commentEl.value
+            reset()
+            index += 1
+            commentEl.value = ""
+        }
     }
 }
 
