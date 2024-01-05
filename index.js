ccbe7118a8784503057374e2aa8e2280f4ba8b0c
dedsecrattle <itsprabxxx@gmail.com>
2024-01-05 21:04:29 +0530

    Added Saving Feature

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let count = 0
 let countEl = document.getElementById("count-el")
+let tableEl = document.getElementById("hist-el")
+let index = 1
 
 function increment() {
     count += 1
@@ -9,4 +11,17 @@ function increment() {
 function reset() {
     count = 0
     countEl.textContent = count
+}
+
+
+function save(){
+    if(count > 0){
+        var row = tableEl.insertRow(index);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.textContent = index
+        cell2.textContent = count
+        reset()
+        index += 1
+    }
 }
